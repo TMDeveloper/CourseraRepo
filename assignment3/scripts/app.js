92a3ceb98970a81de8bcfc4cf38146f1a821f9bf
Vasilis Bilbilis <bilbilisv@gmail.com>
2017-03-04 14:44:45 +0200

    First commit

---
+++ assignment3/scripts/app.js
@@ -0,0 +1,68 @@
+(function () {
+'use strict';
+    
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', foundItemsDirective)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+    
+NarrowItDownController.$inject = ['MenuSearchService'];
+    
+function NarrowItDownController(MenuSearchService) {
+    var narrowedList = this;
+    
+    narrowedList.getFoundItems = function (){
+        narrowedList.found = MenuSearchService.getMatchedMenuItems(narrowedList.searchTerm);
+    };
+    
+    narrowedList.removeItem = function (index){
+        narrowedList.found.splice(index,1);
+    };
+    
+    /*narrowedList.itemsInList = function () {
+        if(narrowedList.found.length == 0 || narrowedList.searchTerm ==""){
+            return true;
+        }
+    };*/
+
+}
+    
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+    var foundItems = [];
+    
+    service.getMatchedMenuItems = function (searchTerm) {
+        return $http({
+            method: 'GET',
+            url: (ApiBasePath + "/menu_items.json")
+        }).then(function (result) {
+           var allItems = result.data.menu_items;
+            for(var i=0; i< allItems.length; i++){
+                if(allItems[i].description.indexOf(searchTerm.toLowerCase()) != -1){
+                    foundItems.push(allItems[i]);
+                }
+            }
+            return foundItems;
+        })
+        .catch(function(error){
+            console.log('Something went wrong...')
+        })
+        
+    };
+}
+    
+function foundItemsDirective () {
+   var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      found: '<',
+      onRemove: '&'
+    }
+  };
+
+  return ddo;
+}
+    
+})();
